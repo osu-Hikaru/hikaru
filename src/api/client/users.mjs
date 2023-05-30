4ebc2b1d212a0cc8c0809af9d3cc6a62c9ae7a33
WitchOfFrost <github@emilia-tan.com>
2023-05-30 15:24:06 +0200

    Fixed signups

--- src/api/client/users.mjs
+++ src/api/client/users.mjs
@@ -9,31 +9,35 @@ const database = global.database;
 export const POST = async (req, res) => {
   try {
     if (
-      !req.fields.user ||
-      !req.fields.user.username ||
-      !req.fields.user.password ||
-      !req.fields.user.user_email
+      !req.fields["user[username]"] ||
+      !req.fields["user[user_email]"] ||
+      !req.fields["user[password]"]
     ) {
       res.status(400);
       res.send();
       return;
     } else if (
-      /^\w+$/gm.test(req.fields.user.username) &&
-      /^[a-zA-Z0-9_!?\-%&$]+$/gm.test(req.fields.user.password) &&
+      /^\w+$/gm.test(req.fields["user[username]"]) &&
+      /^[a-zA-Z0-9_!?\-%&$]+$/gm.test(req.fields["user[password]"]) &&
       /[a-zA-Z0-9_!?\-%&$]+(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g.test(
-        req.fields.user.user_email
+        req.fields["user[user_email]"]
       )
     ) {
       const salt = await bcrypt.genSaltSync(10);
-      const hash = await bcrypt.hashSync(req.fields.user.password, salt);
+      const hash = await bcrypt.hashSync(req.fields["user[password]"], salt);
 
-      await database.runQuery(
+      const newUser = await database.runQuery(
         `INSERT INTO accounts (username, email, password) VALUES (?,?,?)`,
         [
-          String(req.fields.user.username),
-          String(req.fields.user.user_email),
+          String(req.fields["user[username]"]),
+          String(req.fields["user[user_email]"]),
           String(hash),
         ]
+      );
+
+      await database.runQuery(
+        `INSERT INTO users (user_id, username) VALUES (?, ?)`,
+        [Number(newUser.insertId), String(req.fields["user[username]"])]
       );
 
       res.status(200);
@@ -45,7 +49,7 @@ export const POST = async (req, res) => {
       return;
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500);
     res.send();
     return;
