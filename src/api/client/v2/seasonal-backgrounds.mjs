6d7f5b9573c7a92f79a546b4ab8f517a3b303486
WitchOfFrost <github@emilia-tan.com>
2023-06-05 10:54:22 +0200

    Copyright notices as well as formatting

--- src/api/client/v2/seasonal-backgrounds.mjs
+++ src/api/client/v2/seasonal-backgrounds.mjs
@@ -1,6 +1,6 @@
 // Licensed under GPL v3 - Check Repository Root for full License notice.
 // osu!Hikaru, a fully independent osu!Lazer Private Server backend.
-// Copyright (C) 2021 Hikaru Team <copyright@hikaru.pw>
+// Copyright (C) 2023 Hikaru Team <copyright@hikaru.pw>
 
 export const GET = async (req, res) => {
   try {
