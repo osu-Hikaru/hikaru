1c11a9a47e7a149bd578f673eaf56b9c404416f5
WitchOfFrost <github@emilia-tan.com>
2024-05-23 08:31:02 +0200

    Cleanup old files

--- src/services/util.service.ts
+++
@@ -1,8 +0,0 @@
-import { randomBytes } from 'crypto';
-
-export const genSecureHexString = (length: number) => {
-    const bytes = Math.ceil(length / 2);
-    const buffer = randomBytes(bytes);
-    const hexString = buffer.toString('hex').slice(0, length);
-    return hexString;
-};
