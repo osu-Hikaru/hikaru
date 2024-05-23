281cea933b0679400e6036018b6276af5e880c9b
WitchOfFrost <github@emilia-tan.com>
2024-05-23 08:35:12 +0200

    Adding rudimentary /api/v2/notifications endpoint to prevent client crash

---
+++ apps/backend/src/controllers/api/v2/notifications.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+export const get = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res
+      .status(200)
+      .json({
+        notification_endpoint: "wss://notify.hikaru.pw",
+        notifications: [],
+      });
+  } catch (e) {
+    console.error(`Error: ${e}`);
+    next(e);
+  }
+};
