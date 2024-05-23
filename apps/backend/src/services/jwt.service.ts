f1e0cdeb573f9728d388666bc655d4b35001c4da
WitchOfFrost <github@emilia-tan.com>
2024-05-23 10:45:54 +0200

    added jwt verification

--- apps/backend/src/services/jwt.service.ts
+++ apps/backend/src/services/jwt.service.ts
@@ -1,22 +1,30 @@
 import { readFileSync } from "node:fs";
-import jwt from "jsonwebtoken";
+import pkg from "jsonwebtoken";
+
+const { verify: jwtVerify, sign: jwtSign } = pkg;
 
 export default class JwtService {
   private static instance: JwtService;
-  private static key: Buffer;
+  private static private_key: Buffer;
+  private static public_key: Buffer;
 
   private constructor() {
     if (!JwtService.instance) {
       JwtService.instance = this;
     }
 
-    JwtService.key = readFileSync("./keys/private.key");
+    JwtService.private_key = readFileSync("./keys/private.key");
+    JwtService.public_key = readFileSync("./keys/public.key");
 
     return JwtService.instance;
   }
 
   public sign(data: any): string {
-    return jwt.sign(data, JwtService.key, { algorithm: "RS256" });
+    return jwtSign(data, JwtService.private_key, { algorithm: "RS256" });
+  }
+
+  public verify(jwt: string): any {
+    return jwtVerify(jwt, JwtService.public_key)
   }
 
   public static getInstance(): JwtService {
