6518c7ae14a57706dbe8cc838f31d17e5b72e3e4
WitchOfFrost <github@emilia-tan.com>
2024-08-03 22:57:27 +0200

    Refresh tokens, anyone?

--- apps/backend/src/controllers/oauth/token.controller.ts
+++ apps/backend/src/controllers/oauth/token.controller.ts
@@ -1,14 +1,15 @@
 // Libraries
-import { Request, Response, NextFunction } from "express";
+import {NextFunction, Request, Response} from "express";
 
 // Models
-import { Account } from "../../models/account.model.js";
+import {Account} from "../../models/account.model.js";
 
 // Services
-import JwtService from "../../services/jwt.service.js";
+import JwtService, {jwtData} from "../../services/jwt.service.js";
 
 // Errors
-import { BadRequest, Forbidden } from "../../errors/api.error.js";
+import {BadRequest, Forbidden} from "../../errors/api.error.js";
+
 
 /**
  * Handles the POST request for generating an authentication token.
@@ -19,24 +20,66 @@ import { BadRequest, Forbidden } from "../../errors/api.error.js";
  * @returns {Promise<void>} - A Promise that resolves when the request is handled.
  */
 export const post = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const formData = req.body;
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> => {
 
+        const formData = req.body;
+        console.log(formData)
+        switch (formData.grant_type) {
+            case "password":
+                await authorizePassword(formData).then(jwt => {
+                    if (jwt) {
+                        res
+                            .cookie("jwt", jwt.token, {
+                                maxAge: jwt.data.exp,
+                            })
+                            .status(200)
+                            .json({
+                                access_token: jwt.token,
+                                expires_in: jwt.data.exp - Math.floor(Date.now() / 1000),
+                                refresh_token: jwt.data.jti,
+                                token_type: "Bearer",
+                            });
+                    }
+                }).catch(e => {
+                    next(e);
+                })
+                break;
+            case "refresh_token":
+                await authorizeRefreshToken(formData).then(jwt => {
+                    if (jwt) {
+                        res
+                            .cookie("jwt", jwt.token, {
+                                maxAge: jwt.data.exp,
+                            })
+                            .status(200)
+                            .json({
+                                access_token: jwt.token,
+                                expires_in: jwt.data.exp - Math.floor(Date.now() / 1000),
+                                refresh_token: jwt.data.jti,
+                                token_type: "Bearer",
+                            });
+                    }
+                })
+                break;
+        }
+    }
+;
+
+const authorizePassword = async (formData: any): Promise<jwtData | undefined> => {
     // Check if the client_id and client_secret are provided
     if (
-      (Number(process.env.CLIENT_ID) ?? 0) !== Number(formData.client_id) ||
-      (process.env.CLIENT_SECRET ?? "") !== formData.client_secret
+        (Number(process.env.CLIENT_ID) ?? 0) !== Number(formData.client_id) ||
+        (process.env.CLIENT_SECRET ?? "") !== formData.client_secret
     ) {
-      throw new Forbidden("Invalid client credentials.");
+        throw new Forbidden("Invalid client credentials.");
     }
 
     // Check if the username and password are provided
     if (!formData.username || !formData.password) {
-      throw new BadRequest("Username and password are required");
+        throw new BadRequest("Username and password are required");
     }
 
     // Create a new Account object
@@ -45,7 +88,7 @@ export const post = async (
 
     // If the username or password is invalid, throw a Forbidden error
     if (!valid) {
-      throw new Forbidden("Invalid username or password.");
+        throw new Forbidden("Invalid username or password.");
     }
 
     /**
@@ -53,23 +96,36 @@ export const post = async (
      *
      * @type {string}
      */
-    const jwt = JwtService.getInstance().createJWT(
-      req.body.client_id,
-      contextUser.getId()
+    return JwtService.getInstance().createJWT(
+        formData.client_id,
+        contextUser.getId()
     );
+}
 
-    res
-      .cookie("jwt", jwt.token, {
-        maxAge: jwt.data.exp,
-      })
-      .status(200)
-      .json({
-        access_token: jwt.token,
-        expires_in: jwt.data.exp - Math.floor(Date.now() / 1000),
-        refresh_token: jwt.data.jti,
-        token_type: "Bearer",
-      });
-  } catch (e) {
-    next(e);
-  }
-};
+const authorizeRefreshToken = async (formData: any): Promise<jwtData | undefined> => {
+    // Check if the client_id and client_secret are provided
+    if (
+        (Number(process.env.CLIENT_ID) ?? 0) !== Number(formData.client_id) ||
+        (process.env.CLIENT_SECRET ?? "") !== formData.client_secret
+    ) {
+        throw new Forbidden("Invalid client credentials.");
+    }
+
+    // Check if the refresh token was provided
+    if (!formData.refresh_token) {
+        throw new BadRequest("Username and password are required");
+    }
+
+    // Verify whether the JWT is valid
+    const decodedToken = JwtService.getInstance().verify(Buffer.from(formData.refresh_token, 'base64').toString('ascii'));
+
+    /**
+     * JSON Web Token (JWT) representing the authentication token.
+     *
+     * @type {string}
+     */
+    return JwtService.getInstance().createJWT(
+        formData.client_id,
+        decodedToken.sub
+    );
+}
--- apps/backend/src/services/jwt.service.ts
+++ apps/backend/src/services/jwt.service.ts
@@ -1,105 +1,113 @@
 // Libraries
-import { readFileSync } from "node:fs";
+import {readFileSync} from "node:fs";
 import pkg from "jsonwebtoken";
-
-const { verify: jwtVerify, sign: jwtSign } = pkg;
-
 // Services
-import { Service } from "./service.js";
-import { genSecureHexString } from "./util.service.js";
+import {Service} from "./service.js";
+import {genSecureHexString} from "./util.service.js";
 
-// Interfaces
-interface jwtData {
-  token: string;
-  data: {
-    aud: string;
-    jti: string;
-    iat: number;
-    nbf: number;
-    exp: number;
-    sub: number;
-    scopes: Array<string>;
-  };
+const {verify: jwtVerify, sign: jwtSign} = pkg;
+
+// Types
+export type jwtData = {
+    token: string;
+    data: {
+        aud: string;
+        jti: string;
+        iat: number;
+        nbf: number;
+        exp: number;
+        sub: number;
+        scopes: Array<string>;
+    };
 }
 
 /**
  * Service for handling JSON Web Tokens (JWT).
  */
 export default class JwtService extends Service {
-  private static instance: JwtService;
-  private static private_key: Buffer;
-  private static public_key: Buffer;
+    private static instance: JwtService;
+    private static private_key: Buffer;
+    private static public_key: Buffer;
 
-  /**
-   * Private constructor to enforce singleton pattern.
-   */
-  private constructor() {
-    super();
+    /**
+     * Private constructor to enforce singleton pattern.
+     */
+    private constructor() {
+        super();
 
-    if (!JwtService.instance) {
-      JwtService.instance = this;
+        if (!JwtService.instance) {
+            JwtService.instance = this;
+        }
+
+        JwtService.private_key = readFileSync("./keys/private.key");
+        JwtService.public_key = readFileSync("./keys/public.key");
+
+        return JwtService.instance;
     }
 
-    JwtService.private_key = readFileSync("./keys/private.key");
-    JwtService.public_key = readFileSync("./keys/public.key");
+    /**
+     * Returns the singleton instance of the JwtService class.
+     * @returns The singleton instance of JwtService.
+     */
+    public static getInstance(): JwtService {
+        if (!JwtService.instance) {
+            JwtService.instance = new JwtService();
+        }
 
-    return JwtService.instance;
-  }
-
-  /**
-   * Returns the singleton instance of the JwtService class.
-   * @returns The singleton instance of JwtService.
-   */
-  public static getInstance(): JwtService {
-    if (!JwtService.instance) {
-      JwtService.instance = new JwtService();
+        return JwtService.instance;
     }
 
-    return JwtService.instance;
-  }
+    /**
+     * Creates a JSON Web Token (JWT) with the specified client ID and user ID.
+     * @param client_id - The client ID for which the JWT is being created.
+     * @param user_id - The user ID associated with the JWT.
+     * @returns The generated JWT.
+     */
+    public createJWT(client_id: string, user_id: number): jwtData {
+        return this.sign({
+            aud: client_id,
+            jti: Buffer.from(this.sign({
+                aud: client_id,
+                jti: genSecureHexString(80),
+                iat: Math.floor(Date.now() / 1000),
+                nbf: Math.floor(Date.now() / 1000),
+                exp: Math.floor(Date.now() / 1000) + (86400 * 10),
+                sub: user_id,
+                scopes: ["authorization"],
+            }).token).toString('base64'),
+            iat: Math.floor(Date.now() / 1000),
+            nbf: Math.floor(Date.now() / 1000),
+            exp: Math.floor(Date.now() / 1000) + 86400,
+            sub: user_id,
+            scopes: ["*"],
+        });
 
-  /**
-   * Creates a JSON Web Token (JWT) with the specified client ID and user ID.
-   * @param client_id - The client ID for which the JWT is being created.
-   * @param user_id - The user ID associated with the JWT.
-   * @returns The generated JWT.
-   */
-  public createJWT(client_id: string, user_id: number): jwtData {
-    return this.sign({
-      aud: client_id,
-      jti: genSecureHexString(80),
-      iat: Math.floor(Date.now() / 1000),
-      nbf: Math.floor(Date.now() / 1000),
-      exp: Math.floor(Date.now() / 1000) + 86400,
-      sub: user_id,
-      scopes: ["*"],
-    });
-  }
-
-  /**
-   * Signs the provided data and returns a JWT.
-   * @param data - The data to be signed.
-   * @returns The signed JWT.
-   */
-  private sign(data: any): jwtData {
-    return {
-      token: jwtSign(data, JwtService.private_key, { algorithm: "RS256" }),
-      data: data,
-    };
-  }
-
-  /**
-   * Verifies the provided JWT and returns the decoded data.
-   * @param jwt - The JWT to be verified.
-   * @returns The decoded data from the JWT.
-   */
-  public verify(jwt: string): any {
-    try {
-      let token = jwtVerify(jwt, JwtService.public_key);
-
-      return token;
-    } catch (err) {
-      return this.identifyErrorType(err);
     }
-  }
+
+    /**
+     * Signs the provided data and returns a JWT.
+     * @param data - The data to be signed.
+     * @returns The signed JWT.
+     */
+    private sign(data: any): jwtData {
+        return {
+            token: jwtSign(data, JwtService.private_key, {algorithm: "RS256"}),
+            data: data,
+        };
+    }
+
+    /**
+     * Verifies the provided JWT and returns the decoded data.
+     * @param jwt - The JWT to be verified.
+     * @returns The decoded data from the JWT.
+     */
+    public verify(jwt: string): any {
+        try {
+            let token = jwtVerify(jwt, JwtService.public_key);
+
+            return token;
+        } catch (err) {
+            return this.identifyErrorType(err);
+        }
+    }
 }
