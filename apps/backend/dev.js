a63300a352b72b8af56c22545dfd7077fb734acc
WitchOfFrost <github@emilia-tan.com>
2024-08-03 21:03:02 +0200

    Added bridgeppy as submodule

--- apps/backend/dev.js
+++ apps/backend/dev.js
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename);
 nodemon({
   script: path.join(__dirname, "src/index.ts"),
   watch: path.join(__dirname, "src/**"),
+  inspect: true,
   ext: "ts,json",
   exec: `node --trace-warnings --env-file=development.env --loader ts-node/esm ${path.join(__dirname, "src/index.ts")} | pino-pretty`
 });
