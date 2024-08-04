c52c27911fd037b93407ed4fa9640d3fa043bb7e
WitchOfFrost <github@emilia-tan.com>
2024-08-04 16:55:16 +0200

    Moved the final things

--- models/groups.model.ts
+++ models/groups.model.ts
@@ -1,81 +1,81 @@
-import { DatabaseModel } from "./model.js";
+import {DatabaseModel} from "./model.js";
 
-import { Group } from "../classes/group.class.js";
+import {Group} from "../barrel.js";
 
 export class GroupsModel extends DatabaseModel {
-  private static instance: GroupsModel;
-  private groups: Group[] = [];
+    private static instance: GroupsModel;
+    private groups: Group[] = [];
 
-  private constructor() {
-    super();
+    private constructor() {
+        super();
 
-    if (!GroupsModel.instance) {
-      GroupsModel.instance = this;
+        if (!GroupsModel.instance) {
+            GroupsModel.instance = this;
+        }
+
+        return GroupsModel.instance;
     }
 
-    return GroupsModel.instance;
-  }
-
-  public static getInstance(): GroupsModel {
-    if (!GroupsModel.instance) {
-      GroupsModel.instance = new GroupsModel();
+    public static getInstance(): GroupsModel {
+        if (!GroupsModel.instance) {
+            GroupsModel.instance = new GroupsModel();
+        }
+        return GroupsModel.instance;
     }
-    return GroupsModel.instance;
-  }
 
-  public clearCache(): void {
-    this.groups = [];
-  }
+    public clearCache(): void {
+        this.groups = [];
+    }
 
-  public getGroups(ids: Array<number>): Promise<Group[]> {
-    const promises: Array<Promise<Group>> = [];
+    public getGroups(ids: Array<number>): Promise<Group[]> {
+        const promises: Array<Promise<Group>> = [];
 
-    ids.forEach((groupid) => {
-      promises.push(this.fetchGroup(groupid));
-    });
+        ids.forEach((groupid) => {
+            promises.push(this.fetchGroup(groupid));
+        });
 
-    return Promise.all(promises).then((groups) => {
-      return groups;
-    });
-  }
+        return Promise.all(promises).then((groups) => {
+            return groups;
+        });
+    }
 
-  private async fetchGroup(id: number): Promise<Group> {
-    return new Promise(async (resolve, reject) => {
-      const lookupGroup = this.groups.find((group) => group.id === id);
+    private async fetchGroup(id: number): Promise<Group> {
+        return new Promise(async (resolve, reject) => {
+            const lookupGroup = this.groups.find((group) => group.id === id);
 
-      if (lookupGroup) {
-        resolve(JSON.parse(lookupGroup.getObject()));
-      } else {
-        const dbService = this._databaseService.getClient();
+            if (lookupGroup) {
+                resolve(JSON.parse(lookupGroup.getObject()));
+            } else {
+                const dbService = this._databaseService.getClient();
 
-        await dbService.groups
-          .findFirstOrThrow({
-            where: {
-              id: id,
-            },
-          })
-          .then((group) => {
-            const newGroup = new Group();
+                await dbService.groups
+                    .findFirstOrThrow({
+                        where: {
+                            id: id,
+                        },
+                    })
+                    .then((group) => {
+                        const newGroup = new Group();
 
-            Object.keys(group).forEach((key) => {
-              if (this.isValidKey(key, newGroup)) {
-                (newGroup as any)[key] = (group as any)[key];
-              }
-            });
-            this.groups.push(newGroup);
-            resolve(JSON.parse(newGroup.getObject()));
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      }
-    });
-  }
+                        Object.keys(group).forEach((key) => {
+                            if (this.isValidKey(key, newGroup)) {
+                                (newGroup as any)[key] = (group as any)[key];
+                            }
+                        });
+                        this.groups.push(newGroup);
+                        resolve(JSON.parse(newGroup.getObject()));
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            }
+        });
+    }
 
-  private isValidKey(
-    key: string | number | symbol,
-    instance: any
-  ): key is keyof typeof instance {
-    return key in instance;
-  }
+    private isValidKey(
+        key: string | number | symbol,
+        instance: any
+    ): key is keyof typeof instance {
+        return key in instance;
+    }
 }
